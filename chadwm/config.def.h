5207af520930f9e158cfa7f05a066a1fb34acafd
Youssef K <youssefmkachkache@gmail.com>
2025-04-24 17:21:52 +0100

    update layout keymaps

--- chadwm/config.def.h
+++ chadwm/config.def.h
@@ -220,10 +220,9 @@ static const Key keys[] = {
 
     // layout
     { MODKEY,                           XK_t,       setlayout,      {.v = &layouts[0]} },
-    { MODKEY|ShiftMask,                 XK_f,       setlayout,      {.v = &layouts[1]} },
-    { MODKEY,                           XK_m,       setlayout,      {.v = &layouts[2]} },
+    { MODKEY,                           XK_m,       setlayout,      {.v = &layouts[1]} },
     { MODKEY|ControlMask,               XK_g,       setlayout,      {.v = &layouts[10]} },
-    { MODKEY|ControlMask|ShiftMask,     XK_t,       setlayout,      {.v = &layouts[13]} },
+    { MODKEY|ShiftMask,                 XK_f,       setlayout,      {.v = &layouts[13]} },
     { WINKEY,                           XK_space,   setlayout,      {0} },
     { MODKEY|ControlMask,               XK_comma,   cyclelayout,    {.i = -1 } },
     { MODKEY|ControlMask,               XK_period,  cyclelayout,    {.i = +1 } },
